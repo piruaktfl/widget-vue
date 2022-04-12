2e1e3a38450fe6f274553aa58e88241eba3c9f86
Thiago Furlan <iagokorn@yahoo.com.br>
2022-04-12 15:17:42 -0300

    tete

--- serve.js
+++ serve.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 const port = process.env.PORT || 8080;
 const app = express();
 
+app.use(cors())
 app.use(express.static(__dirname + "/dist/"));
 app.get(/.*/, function (req, res) {
   res.sendFile(__dirname + "/dist/index.html");
