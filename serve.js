f9e2d94073d1aa048fdda937e0ca723a60647548
Thiago Furlan <iagokorn@yahoo.com.br>
2022-04-12 15:36:41 -0300

    tete

--- serve.js
+++ serve.js
@@ -4,8 +4,14 @@ const port = process.env.PORT || 8080;
 const app = express();
 
 app.use(cors())
+const options = {
+  origin: true,
+  methods: ["POST","GET", "PUT", "DELETE", "PATCH"],
+  credentials: true,
+  maxAge: 3600
+};
 app.use(express.static(__dirname + "/dist/"));
-app.get(/.*/, function (req, res) {
+app.get(/.*/, cors(options),function (req, res) {
   res.sendFile(__dirname + "/dist/index.html");
 });
 
