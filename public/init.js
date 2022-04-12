c802109bc84939fc8e44ee324acbddc2531fa1ba
Thiago Furlan <iagokorn@yahoo.com.br>
2022-04-12 15:24:52 -0300

    tete

--- public/init.js
+++ public/init.js
@@ -1,35 +1,35 @@
-function init(teste) {
+// function init(teste) {
 
-  function handlerLoadWidget() {
+//   function handlerLoadWidget() {
 
-    const WIDGET_URL = "https://widget-vue.herokuapp.com/"
-    const iframe = document.createElement('iframe')
-    console.log(WIDGET_URL)
+//     const WIDGET_URL = "https://widget-vue.herokuapp.com/"
+//     const iframe = document.createElement('iframe')
+//     console.log(WIDGET_URL)
 
-    iframe.src(WIDGET_URL)
-    iframe.id = 'widget-ns'
-    iframe.style.position = 'fixed'
-    iframe.style.bottom = '0px'
-    iframe.style.left = '0px'
-    iframe.style.overflow = 'hidden'
-    iframe.style.border = '0px'
-    iframe.style.zIndex = '99999'
+//     iframe.src(WIDGET_URL)
+//     iframe.id = 'widget-ns'
+//     iframe.style.position = 'fixed'
+//     iframe.style.bottom = '0px'
+//     iframe.style.left = '0px'
+//     iframe.style.overflow = 'hidden'
+//     iframe.style.border = '0px'
+//     iframe.style.zIndex = '99999'
 
-    document.body.appendChild(iframe)
+//     document.body.appendChild(iframe)
 
-    // window.addEventListener('message', (event) => {
-    //   if(!event.data.isWiget) return
-    //   // if (event.data.isOpen) {
-    //   //   iframe.width='100%'
-    //   //   iframe.height='100%'
-    //   // }
-    // })
-    // return
-  }
-  const script = document.createElement('script');
+//     // window.addEventListener('message', (event) => {
+//     //   if(!event.data.isWiget) return
+//     //   // if (event.data.isOpen) {
+//     //   //   iframe.width='100%'
+//     //   //   iframe.height='100%'
+//     //   // }
+//     // })
+//     // return
+//   }
+//   const script = document.createElement('script');
 
-  script.addEventListener('load', handlerLoadWidget)
-  document.body.appendChild(script) 
-}
+//   script.addEventListener('load', handlerLoadWidget)
+//   document.body.appendChild(script) 
+// }
 
-window.init = init
+// window.init = init
