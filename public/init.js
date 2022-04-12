a89cf937a4be4158cc78d5a52852d91c55c09d5b
Thiago Furlan <iagokorn@yahoo.com.br>
2022-04-12 11:21:50 -0300

    init.js

---
+++ public/init.js
@@ -0,0 +1,34 @@
+function init(teste) {
+
+  function handlerLoadWidget() {
+
+    const WIDGET_URL = "https://widget-vue.herokuapp.com/"
+    const iframe = document.createElement('iframe')
+
+    iframe.src(WIDGET_URL)
+    iframe.id = 'widget-ns'
+    iframe.style.position = 'fixed'
+    iframe.style.bottom = '0px'
+    iframe.style.left = '0px'
+    iframe.style.overflow = 'hidden'
+    iframe.style.border = '0px'
+    iframe.style.zIndex = '99999'
+
+    document.body.appendChild(iframe)
+
+    window.addEventListener('message', (event) => {
+      if(!event.data.isWiget) return
+      // if (event.data.isOpen) {
+      //   iframe.width='100%'
+      //   iframe.height='100%'
+      // }
+    })
+    return
+  }
+  const script = document.createElement('script');
+
+  script.addEventListener('load', handlerLoadWidget)
+  document.body.appendChild(script) 
+}
+
+window.init = init
